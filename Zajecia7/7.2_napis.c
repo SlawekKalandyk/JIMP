0be9d142312e631c9cdc2f26e5575be8e6154984
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2018-01-05 00:36:44 +0100

    7.2 zrobione

--- Zajecia7/7.2_napis.c
+++ Zajecia7/7.2_napis.c
@@ -1,27 +1,53 @@
 //Sławomir Kalandyk
-//do zrobienia
+
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
 
-void printLine(char *napis, ...);
+void printLine(char *napis, int *tab, int rozmiarTab);
 
 int main(void)
 {
-    char *n = "Podaj pierwszy bok trójkąta: %d\n\
-    Podaj drugi bok trójkąta: %d\n\
-    Podaj trzeci bok trójkąta: %d\n";
-    printLine(n);
+    char *tekst = "Podaj pierwszy bok trójkąta: %dPodaj drugi bok trójkąta: %dPodaj trzeci bok trójkąta: %d\n";
+    int *tab;
+    int poczatkowyRozmiarTab = 1;
+
+    tab = (int*)malloc(poczatkowyRozmiarTab * sizeof(tab[0]));
+    printLine(tekst, tab, poczatkowyRozmiarTab);
+
+    free(tab);
 
     return 0;
 }
 
-void printLine(char *napis, ...)
+void printLine(char *napis, int *tab, int rozmiarTab)
 {
-    //double val;
-    //va_list vl;
-    //va_start(vl, napis);
+    int licznik = 0;
+    int index = 0;
+    int *check = 0;
 
-    //scanf("%d", &val);
+    for(unsigned int i = 0; i < strlen(napis) - 1; i++)
+    {
+        if(napis[i] == '%' && napis[i + 1] == 'd')
+        {
+            for(unsigned int j = licznik; j < i; j++)
+                printf("%c", napis[j]);
 
-    //va_end(vl);
+            scanf("%d", &tab[index]);
+            index += 1;
+            licznik = i + 2;
+            check = realloc(tab, (rozmiarTab + index) * sizeof(tab[0]));
+
+            if(!check)
+            {
+                free(tab);
+                printf("Wystąpił błąd w zwiększaniu rozmiaru tablicy zmiennych\n");
+            }
+            
+        }
+    }
+
+    for(unsigned int j = licznik; j < strlen(napis); j++)
+        printf("%c", napis[j]);
 }
