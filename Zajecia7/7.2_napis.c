b7ddd375d3435aa21c7ce237675ceb92cc7d2327
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2018-01-05 00:50:48 +0100

    7.2 gotowe

--- Zajecia7/7.2_napis.c
+++ Zajecia7/7.2_napis.c
@@ -1,7 +1,9 @@
 //Sławomir Kalandyk
-
+//Nie potrafię usunąc newline'a przy scanfie, przez co przy każdym wczytaniu liczby 
+//program przechodzi do nowej linii.
+//Nie zastosowałem też ..., ponieważ nie znalazłem sposobu na przypisanie wartości 
+//do argumentów z listy i po wielu próbach zostawiłem to i wybrałem prostszą dla mnie metodę rozwiązania.
 #include <stdio.h>
-#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -9,13 +11,13 @@ void printLine(char *napis, int *tab, int *rozmiarTab);
 
 int main(void)
 {
-    char *tekst = "Podaj pierwszy bok trójkąta: %dPodaj drugi bok trójkąta: %dPodaj trzeci bok trójkąta: %d\n";
+    char *tekst = "Podaj pierwszy bok trójkąta: %dPodaj drugi bok trójkąta: %dPodaj trzeci bok trójkąta: %d";
     int *tab;
     int rozmiarTab = 0;
-
+    
     tab = (int*)malloc(rozmiarTab * sizeof(tab[0]));
     printLine(tekst, tab, &rozmiarTab);
-
+    
     free(tab);
 
     return 0;
@@ -23,7 +25,7 @@ int main(void)
 
 void printLine(char *napis, int *tab, int *rozmiarTab)
 {
-    int licznik = 0;
+    int licznikZnakow = 0;
     int index = 0;
     int *check = 0;
 
@@ -31,13 +33,13 @@ void printLine(char *napis, int *tab, int *rozmiarTab)
     {
         if(napis[i] == '%' && napis[i + 1] == 'd')
         {
-            for(unsigned int j = licznik; j < i; j++)
+            for(unsigned int j = licznikZnakow; j < i; j++)
                 printf("%c", napis[j]);
 
             scanf("%d", &tab[index]);
             index += 1;
             *rozmiarTab += 1;
-            licznik = i + 2;
+            licznikZnakow = i + 2;
             check = realloc(tab, *rozmiarTab * sizeof(tab[0]));
 
             if(!check)
@@ -48,6 +50,6 @@ void printLine(char *napis, int *tab, int *rozmiarTab)
         }
     }
 
-    for(unsigned int j = licznik; j < strlen(napis); j++)
+    for(unsigned int j = licznikZnakow; j < strlen(napis); j++)
         printf("%c", napis[j]);
 }
